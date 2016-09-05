9ce8e63429f5fb0fe98662441659274f484e5b89
mark <mark@imikoko.com>
2016-09-05 16:50:27 +0800

    laravel由5.0升级到5.1

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -28,7 +28,7 @@ require __DIR__.'/../app/functions.php';
 |
 */
 
-$compiledPath = __DIR__.'/../vendor/compiled.php';
+$compiledPath = __DIR__.'/cache/compiled.php';
 
 if (file_exists($compiledPath))
 {
