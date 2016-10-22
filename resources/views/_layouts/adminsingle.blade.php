d43d9c3e4fa221cfe81573dc8f2f2061159efacb
mark <mark@imikoko.com>
2016-10-22 16:09:12 +0800

    删除无用的文件

--- resources/views/_layouts/adminsingle.blade.php
+++ resources/views/_layouts/adminsingle.blade.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('parts.admin.head');
-<body class="admin_body">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mark的私人博客</title>
 
+	<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/admin/admin.css') }}" rel="stylesheet">
+	<link href="{{ asset('/fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<script src="{{ asset('/js/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
+</head>
+<body class="admin_body">
 	@yield('content')
 
 	<!-- Scripts -->
